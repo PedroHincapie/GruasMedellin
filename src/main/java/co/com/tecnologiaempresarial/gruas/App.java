2fb06cfeda70a83c65c2695262c2fd3a2016c5ea
PEDRO JESÚS HINCAPIÉ GARCIA <PHINCAPIE_EXT@XM.COM.CO>
2018-04-19 22:20:12 -0500

    Contruccion de inicializadores de la aplicacion
    Co-Authored-By: Pedro Jesus Hincapie Garcia <pedrohincapie@users.noreply.github.com>

--- src/main/java/co/com/tecnologiaempresarial/gruas/App.java
+++ src/main/java/co/com/tecnologiaempresarial/gruas/App.java
@@ -1,13 +1,19 @@
 package co.com.tecnologiaempresarial.gruas;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages="co.com.tecnologiaempresarial.gruas.*")
 public class App 
 {
     public static void main( String[] args )
     {
+    	SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
