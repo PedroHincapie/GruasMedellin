0dab46795e67e58da6bd9c7572d67ba331926649
PEDRO JESÚS HINCAPIÉ GARCIA <PHINCAPIE_EXT@XM.COM.CO>
2018-04-19 21:34:12 -0500

    Crear Script Creacion de tablas

---
+++ src/main/resorce/db/initBD.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE motos_gruas;
+
+use motos_gruas;
+
+
+CREATE TABLE clientes
+(
+id_cliente INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+nom_cliente VARCHAR(50) NOT NULL,
+telefono VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE motos
+(
+id_moto INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+marca  VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE servicios
+(
+id_servicio INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+id_moto INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+id_usuario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+ubicacion VARCHAR(50) NOT NULL,
+destino VARCHAR(50) NOT NULL,
+costo DOUBLE(7,1) NOT NULL,
+estado VARCHAR(50) NOT NULL
+FOREIGN KEY (id_moto) REFERENCES motos(id_moto),
+FOREIGN KEY (id_usuario) REFERENCES clientes(id_usuario)
+);
